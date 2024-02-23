a5da1a5b6ec6639579a090101fc5db91a8b13d9c
Anusha Uppu <admin@Anusha-Uppu.local>
2024-02-23 13:55:47 +0530

    cat is created

---
+++ cat.py
@@ -0,0 +1,11 @@
+class Cat:
+    def eat(self):
+      print("I can Eat")
+    def sleep(self):
+      print("I can Sleep")
+    def meow(self):
+       print("I can Meow cutely")
+    def scratching(self):
+       print("I can Scratch")
+    def run(self):
+       print("I can Run")  
